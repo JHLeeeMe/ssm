e04b726e991d62ce5c8e2721fd5aa8b8908a2358
JHLeeeMe-T420 <lejung92@gmail.com>
2021-02-05 11:03:26 +0900

    [Update] 문자열 보내기 일부 작성, 미완
    	수정함:        ssm/__init__.py
    	수정함:        ssm/client.py
    	수정함:        ssm/server.py

--- ssm/client.py
+++ ssm/client.py
@@ -1,8 +1,33 @@
 """client
 """
 
+import time
 import socket
+import pickle
+import threading
 
 
 class ScreenMirrorClient:
-    pass
+    def __init__(self, host: str, port: int = 7890):
+        self._host = host
+        self._port = port
+        self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def _send(self):
+        while True:
+            try:
+                data = 'Hello, Socket!'
+                b_data = pickle.dumps(data)
+                self._client_socket.sendall(b_data)
+
+                time.sleep(1)
+            except Exception as e:
+                print(e)
+                break
+
+    def start(self):
+        self._client_socket.connect((self._host, self._port))
+        t = threading.Thread(target=self._send)
+        t.start()
+        t.join()
+
--- ssm/server.py
+++ ssm/server.py
@@ -1,8 +1,50 @@
 """server
 """
 
+import time
 import socket
+import pickle
+import threading
 
 
 class ScreenMirrorServer:
-    pass
+    def __init__(self, host: str, port: int = 7890, conn_limits: int = 5):
+        self._HOST = host
+        self._PORT = port
+
+        self._lock = threading.Lock()
+        self._conn_limits = conn_limits
+
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.bind((self._HOST, self._PORT))
+
+    def _recieve(self, conn_socket: socket.socket, addr: (str, int)):
+        while True:
+            try:
+                b_data = conn_socket.recv(1024)
+                if b_data == b'':
+                    break
+                data = pickle.loads(b_data)
+                print(f'{addr}: {data}')
+            except Exception as e:
+                print(e)
+                break
+
+        with self._lock:
+            self._conn_limits += 1
+        print('thread end')
+
+    def start(self):
+        self._server_socket.listen()
+        while True:
+            with self._lock:
+                if self._conn_limits <= 0:
+                    time.sleep(1)
+                    continue
+
+            conn_socket, addr = self._server_socket.accept()
+            with self._lock:
+                self._conn_limits -= 1
+
+            t = threading.Thread(name=f'{addr[1]}', target=self._recieve, args=(conn_socket, addr))
+            t.start()
