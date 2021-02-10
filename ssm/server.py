d2ca9e7d314b7d441643acfa611d0387e82e59ba
JHLeeeMe-T420 <lejung92@gmail.com>
2021-02-10 22:47:18 +0900

    [Update] screen resize 관련 기능 추가
    	수정함:        ssm/client.py
    	수정함:        ssm/server.py
    
            def _screen_size() 추가

--- ssm/server.py
+++ ssm/server.py
@@ -1,19 +1,22 @@
 """server
 """
 
-import time
+import os
 import struct
 import socket
 import pickle
 import threading
 
 import cv2
+from Xlib.display import Display
 
 
 class ScreenMirrorServer:
     def __init__(self, host: str = '', port: int = 7890):
         self._HOST = host
         self._PORT = port
+
+        self._WIDTH, self._HEIGHT = self._screen_size()
 
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind((self._HOST, self._PORT))
@@ -28,7 +31,7 @@ class ScreenMirrorServer:
     def _receive(self, conn_socket: socket.socket, addr: (str, int)):
         assert (conn_socket is not None)
 
-        overhead_size = struct.calcsize('>I')
+        overhead_size = struct.calcsize('>III')
         payload_bin = b''
 
         try:
@@ -42,7 +45,7 @@ class ScreenMirrorServer:
                 packed_payload_size = payload_bin[:overhead_size]
                 payload_bin = payload_bin[overhead_size:]
 
-                payload_size = struct.unpack('>I', packed_payload_size)[0]
+                width, height, payload_size = struct.unpack('>III', packed_payload_size)
 
                 while len(payload_bin) < payload_size:
                     received_data_bin = conn_socket.recv(4096)
@@ -56,6 +59,9 @@ class ScreenMirrorServer:
                 encoded_screen = pickle.loads(encoded_screen_pkl)
                 screen = cv2.imdecode(encoded_screen, flags=cv2.IMREAD_COLOR)
 
+                if (self._WIDTH < width) and (self._HEIGHT < height):
+                    screen = cv2.resize(screen, (self._WIDTH, self._HEIGHT), interpolation=cv2.INTER_AREA)
+
                 cv2.imshow(winname=f'{addr}', mat=screen)
                 if cv2.waitKey(1) == 27:
                     raise StopIteration
@@ -64,3 +70,7 @@ class ScreenMirrorServer:
             cv2.destroyWindow(winname=f'{addr}')
             print('Mirroring ends...')
 
+    def _screen_size(self) -> (int, int):
+        display = Display(display=os.environ['DISPLAY'])
+        width, height = display.screen().width_in_pixels, display.screen().height_in_pixels
+        return width, height
