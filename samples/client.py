4c03e1947bb23554506c8f3927cf4799d9c3ed52
JHLeeeMe-T420 <lejung92@gmail.com>
2021-02-13 23:03:46 +0900

    [Modify] 종료키 제거
    	수정함:        samples/client.py

--- samples/client.py
+++ samples/client.py
@@ -11,10 +11,3 @@ client = ScreenMirrorClient('localhost', quality=90, cursor=True)
 t = threading.Thread(target=client.start)
 t.start()
 
-while True:
-    key = ord(input(''))
-    if key == ord('q'):
-        break
-
-sys.exit()
-
