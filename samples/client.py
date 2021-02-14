2f7a9b6f4fa132520fbd08ec22c5925dcdc679ae
JHLeeeMe-T420 <lejung92@gmail.com>
2021-02-14 14:49:31 +0900

    [Modify] thread 관련 코드 제거
    	수정함:        samples/client.py
    	수정함:        samples/server.py

--- samples/client.py
+++ samples/client.py
@@ -1,12 +1,8 @@
 """ Sample Client """
 
-import threading
-
 from ssm import ScreenMirrorClient
 
 
 client = ScreenMirrorClient('localhost', quality=90, cursor=True)
-
-t = threading.Thread(target=client.start)
-t.start()
+client.start()
 
--- samples/server.py
+++ samples/server.py
@@ -1,13 +1,8 @@
 """ Sample Server """
 
-import threading
-
 from ssm import ScreenMirrorServer
 
 
 server = ScreenMirrorServer()
-
-t = threading.Thread(target=server.start)
-t.start()
-t.join()
+server.start()
 
